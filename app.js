5cfda696a844d6c105d5f16731a7f2e970ea88f2
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-07-05 17:33:04 -0300

    levantando servidor

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const { send } = require('process');
+const port = 3040;
+
+const app = express()
+
+app.use(express.static('public'));
+app.listen(port, () => console.log('server running in localhost://' + port));
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
