2742fc158ae78e7e4b4500d23f4073aef57a1bcf
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-07-10 21:37:45 -0300

    add rama, stile

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const port = 3040;
 const app = express()
 
 app.use(express.static('public'));
+
 app.listen(port, () => console.log('server running in localhost://' + port));
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
